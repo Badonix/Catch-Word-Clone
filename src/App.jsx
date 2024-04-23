c50f2a404d9cbbbbbdfe983faf6c326906328637
Nick <nikdanelia@gmail.com>
2024-04-24 00:10:02 +0400

    added tracker

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,31 @@ import NoPage from "./pages/NoPage";
 import Result from "./pages/Result";
 import LeaderBoard from "./pages/LeaderBoard";
 import NewUser from "./pages/NewUser";
+import { useEffect } from "react";
+import axios from "axios";
 export default function App() {
+  const Track = async () => {
+    try {
+      const response = await axios.post(
+        "https://tracker-back-flax.vercel.app/record",
+        {
+          apiKey: "HOrDPZxEF9fQlRycA5AdU2YOFbYpoNesvLYKTlKYvFg=",
+        },
+      );
+      return response;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  };
+
+  useEffect(() => {
+    try {
+      Track();
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
   return (
     <BrowserRouter>
       <Routes>
